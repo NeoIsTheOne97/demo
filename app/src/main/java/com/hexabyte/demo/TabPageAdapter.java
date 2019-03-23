dbdde6ce00a6ce2ad3bb9bfa12dcf6d023221685
NeoIsTheOne97 <47405827+NeoIsTheOne97@users.noreply.github.com>
2019-03-23 09:47:44 +0530

    Initial commit

---
+++ app/src/main/java/com/hexabyte/demo/TabPageAdapter.java
@@ -0,0 +1,38 @@
+package com.hexabyte.demo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabPageAdapter extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public TabPageAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                dietFragment tab1 = new dietFragment();
+                return tab1;
+            case 1:
+                profileFragment tab2 = new profileFragment();
+                return tab2;
+            case 2:
+                reportFragment tab3 = new reportFragment();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
